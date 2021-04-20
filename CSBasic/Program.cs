8d94b3e4a2621d6fe7ac438414d36652bbbf1d51
hnr <nar9342@naver.com>
2021-04-20 11:53:13 +0900

    #1 기본 용어 실습(변수명, 주석 등)

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,15 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            int alpha;
+            int Alpha;
+            //int a★; C#은 이모지로 변수명 지정 못함
+            //int 한글숫자; 변수명으로 한글도 가능은 하지만 쓰지 말것
+
+            //inline 주석
+            /*
+             *  comment
+             */
         }
     }
 }
