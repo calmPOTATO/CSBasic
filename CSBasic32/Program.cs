45f3436cee5db562efcb0d7fcea4926df45129c7
hnr <nar9342@naver.com>
2021-05-04 11:20:29 +0900

    #10 if문, else if문 else 문

--- CSBasic32/Program.cs
+++ CSBasic32/Program.cs
@@ -6,7 +6,49 @@ namespace CSBasic32
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (DateTime.Now.Hour < 11)
+            {
+                Console.WriteLine("아침 먹을 시간입니다.");
+            } else if(DateTime.Now.Hour < 15)
+            {
+                Console.WriteLine("점심 먹을 시간입니다.");
+            } else
+            {
+                Console.WriteLine("저녁 먹을 시간입니다.");
+            }
+
+            double score = 3.5;
+            if(score == 4.5)
+            {
+                Console.WriteLine("신");
+            } else if(score <= 4.2) 
+            {
+                Console.WriteLine("교수님의 사랑");
+            }
+            else if (score <= 3.5)
+            {
+                Console.WriteLine("모범생");
+            }
+            else if (score <= 2.8)
+            {
+                Console.WriteLine("일반인");
+            }
+            else if (score <= 1.5) 
+            {
+                Console.WriteLine("오락의 신");
+            }
+            else if (score <= 1.0)
+            {
+                Console.WriteLine("불가촉천민");
+            }
+            else if (score <= 0.5)
+            {
+                Console.WriteLine("자벌레");
+            }
+            else if (score <= 0)
+            {
+                Console.WriteLine("플랑크톤");
+            }
         }
     }
 }
