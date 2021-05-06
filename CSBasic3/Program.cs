09f473aba4d9741ec8b4358fb129b26a7cf0f949
hnr <nar9342@naver.com>
2021-05-06 10:20:42 +0900

    #11 반복문(for, while, dowhile)

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -10,6 +10,33 @@ namespace CSBasic3
     {
         static void Main(string[] args)
         {
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray.Length);
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[99]);
+            //Console.WriteLine(intArray[100]);
+            //Console.WriteLine(intArray[-1]);
+
+            int i = 0;
+            int[] intArray2 = { 52, 273, 32, 65, 103 };
+
+            while(i<=intArray2.Length)
+            {
+                Console.WriteLine(i + "번째 출력 )" + intArray2[i]);
+                i++;
+            }
+
+            string input;
+            do
+            {
+                Console.Write("입력 (종료 : exit)");
+                input = Console.ReadLine();
+            } while (input != "exit");
+
+            for(int j = '가'; j<'힣'; j++)
+            {
+                Console.Write((char)j);
+            }
         }
     }
 }
