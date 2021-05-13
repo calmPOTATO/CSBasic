39ad03600047599c3b15caac73b6155a8b0a61b8
hnr <nar9342@naver.com>
2021-05-13 22:29:11 +0900

    #11 배열과 반복문

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -37,6 +37,63 @@ namespace CSBasic3
             {
                 Console.Write((char)j);
             }
+
+
+            int[] intArray3 = { 1, 2, 3, 4, 5, 6 };
+            for(int k = intArray3.Length-1; i>=0; i++ )
+            {
+                Console.WriteLine(intArray3[i]);
+            }
+
+            for(int k=intArray3.Length; k>0; k--)
+            {
+                Console.WriteLine(intArray3[k - 1]);
+            }
+
+            string [] fruits = { "사과", "배", "포도", "딸기", "멜론" };
+
+            for(i = 0; i<10; i++)
+            {
+
+                for(int j=0; j<10-i; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                for(int j=0; j<i+1; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+
+            string input3 = "Potato Tomato";
+            Console.WriteLine(input3.ToUpper());
+            Console.WriteLine(input3.ToLower());
+
+            string dirtyInput = " text um\n\t";
+            Console.WriteLine("[" + dirtyInput + "]");
+            Console.WriteLine("[" + dirtyInput.Trim() + "]");
+
+            string[] foodArrays2 = { "감자", "고구마", "토마토", "가지", "파프리카" };
+            Console.WriteLine(string.Join(" ", foodArrays2));
+            Console.WriteLine(string.Join(", ", foodArrays2));
+
+
+            int x = 1;
+            while(x<50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+
+            }
         }
     }
 }
